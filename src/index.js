bc999436f24ec3fdecee8258bc367690d0115d6a
Chris Black <black@chrisblack.net>
2019-01-18 14:48:00 -0700

    Started working on the DOM update logic
    Just started with simple entries.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,43 @@
 import style from "./main.css";
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { chrisBlack } from './resume.js';
+import {resume as res} from './resume.js';
 
-console.log(chrisBlack.header);
+let keys = Object.getOwnPropertyNames(res);
 
-document.querySelector('#name').textContent = chrisBlack.header.name;
-document.querySelector('#position').textContent = chrisBlack.header.position;
-document.querySelector('#contact').innerHTML = chrisBlack.header.contact.address + '<br>' +
-                                                    chrisBlack.header.contact.phone + '<br>' +
-                                                    '<a href="mailto:' + chrisBlack.header.contact.email +
-                                                    '">' +  chrisBlack.header.contact.email + '</a>';
+document.querySelector('#name').textValue = 'monkey';
+
+keys.forEach(key => {
+
+    // Check if it's not an array
+    if (!Array.isArray(res[key])) {
+        singleInsert(key);
+    } else {
+        // If it is an array, create a UL and loop throug the values
+        
+    }
+
+});
+
+function singleInsert(key) {
+    if (key === 'email' || key === 'portfolio') {
+        // Clear the space
+        document.querySelector('#'+key).textContent = '';
+
+        // Make <a> element
+        var link = document.createElement('a');
+
+        // Href, ID attribute
+        let href = (key === 'email') ? 'mailto:' + res[key] : 'http://' + res[key];
+        link.setAttribute('href', href);
+        link.setAttribute('ID', key + '-link');
+        
+        // Insert a tags, then add link text.
+        document.querySelector('#'+key).appendChild(link);
+        document.querySelector('#'+key+'-link').textContent = res[key];
+        
+    } else {
+        document.querySelector('#'+key).textContent = res[key];
+    }
+
+}
