2e3e58643c38a30a40936da530f9f81decfc3e08
Chris Black <black@chrisblack.net>
2019-06-16 18:05:48 -0700

    Clean up spacing

--- src/index.js
+++ src/index.js
@@ -1,51 +1,46 @@
 import style from "./main.css";
-const res = require('./resume.json');
-var linkifyStr = require('linkifyjs/string');
-
+const resume = require("./resume.json");
+var linkifyStr = require("linkifyjs/string");
 
 /**
  * Route an object or array for further processing.
  * @param {Object} object - The resume JSON file, at first, or a value within that file.
  * @param {element} [parent=document.querySelector('body')] - The parent node to be passed into the next function.
  */
-function objectHandler(object, parent = document.querySelector('body')) {
+function objectHandler(object, parent = document.querySelector("body")) {
+  let keys = Object.getOwnPropertyNames(object);
 
-    let keys = Object.getOwnPropertyNames(object);
-
-    keys.forEach(key => {
-        if (Array.isArray(object[key])) {
-            // Pass object, current key, and parent node.
-            arrayHandler(object, key, parent);            
-        } else {
-            // Pass value of the current key, current key, and parent node.
-            nodeAppender(object[key], key, parent);
-        }
-    });
+  keys.forEach(key => {
+    if (Array.isArray(object[key])) {
+      // Pass object, current key, and parent node.
+      arrayHandler(object, key, parent);
+    } else {
+      // Pass value of the current key, current key, and parent node.
+      nodeAppender(object[key], key, parent);
+    }
+  });
 }
-
-
 
 /**
  * Create a new DOM node from a key / value pair, or reroute if value is an object.
- * @param {string|Object|Array} object - The value of the passed key / value pair. 
- * @param {string} key - The key of the passed key / value pair. 
+ * @param {string|Object|Array} object - The value of the passed key / value pair.
+ * @param {string} key - The key of the passed key / value pair.
  * @param {element} parent - The parent node of created DOM node.
  * @param {element} [type=div] - The type of created DOM node.
  */
-function nodeAppender(object, key, parent, type = 'div') {
-    var newNode = document.createElement(type);
-    // Set element's class to the key (e.g. 'name' or 'skill').
-    newNode.setAttribute('class', key);
-    parent.appendChild(newNode);
-    if (typeof object === 'object') {
-        // Objects go back through the top, with the parent updated.
-        objectHandler(object, newNode);
-    } else {   
-        // If it's not an object, it's ready to display.     
-        newNode.innerHTML = object;
-    }
+function nodeAppender(object, key, parent, type = "div") {
+  var newNode = document.createElement(type);
+  // Set element's class to the key (e.g. 'name' or 'skill').
+  newNode.setAttribute("class", key);
+  parent.appendChild(newNode);
+  if (typeof object === "object") {
+    // Objects go back through the top, with the parent updated.
+    objectHandler(object, newNode);
+  } else {
+    // If it's not an object, it's ready to display.
+    newNode.innerHTML = object;
+  }
 }
-
 
 /**
  * Turn arrays into unordered lists.
@@ -54,16 +49,15 @@ function nodeAppender(object, key, parent, type = 'div') {
  * @param {element} parent - The parent node of the newly crated <ul> element.
  */
 function arrayHandler(object, key, parent) {
+  // All arrays are made into UL.
+  var newList = document.createElement("ul");
+  newList.setAttribute("class", key);
+  parent.appendChild(newList);
 
-    // All arrays are made into UL.
-    var newList = document.createElement('ul');
-    newList.setAttribute('class', key);
-    parent.appendChild(newList);
-
-    // Now loop through the array.
-    object[key].forEach(el => {
-        nodeAppender(el, key, newList, 'li');
-    });
+  // Now loop through the array.
+  object[key].forEach(el => {
+    nodeAppender(el, key, newList, "li");
+  });
 }
 
 /**
@@ -71,14 +65,14 @@ function arrayHandler(object, key, parent) {
  * Newly-created elements of class [0] are to be appended to element [1].
  */
 var layout = [
-    ['.name', 'name'],
-    ['.address', 'address'],
-    ['.position', 'name'],
-    ['.contact', 'contact'],
-    ['.projects', 'main'], 
-    ['.portfolio', 'aside'],
-    ['.skills', 'aside'],
-    ['.footer', 'footer']
+  [".name", "name"],
+  [".address", "address"],
+  [".position", "name"],
+  [".contact", "contact"],
+  [".projects", "main"],
+  [".portfolio", "aside"],
+  [".skills", "aside"],
+  [".footer", "footer"]
 ];
 
 /**
@@ -86,39 +80,45 @@ var layout = [
  * @param {Array} layout - An array of class/element pairs.
  */
 function nodePlacer(layout) {
-    layout.forEach(el => {
-        let parent = document.querySelector(el[1]);
-        let child = document.querySelector(el[0]);
-        // First create section header from each class name.
-        nodeAppender(child.className, 
-                     'section-header section-header-' + child.className, 
-                     parent, 
-                     'h1');
-        // Then append further nodes.
-        parent.appendChild(child);
-    });
+  layout.forEach(el => {
+    let parent = document.querySelector(el[1]);
+    let child = document.querySelector(el[0]);
+    // First create section header from each class name.
+    nodeAppender(
+      child.className,
+      "section-header section-header-" + child.className,
+      parent,
+      "h1"
+    );
+    // Then append further nodes.
+    parent.appendChild(child);
+  });
 }
 
 /**
  * Linkify link strings.
  */
 function linkifier() {
-    // Linkify text in these classes
-    var linkNodes = ['.email', '.portfolio', '.project__link', '.note', '.footer'];
+  // Linkify text in these classes
+  var linkNodes = [
+    ".email",
+    ".portfolio",
+    ".project__link",
+    ".note",
+    ".footer"
+  ];
 
-    linkNodes.forEach(node => {
-        let link = document.querySelectorAll(node);
-        link.forEach(el => {
-            let newLink = linkifyStr(el.textContent);
-            el.innerHTML = newLink;
-        });
-        
-    })
+  linkNodes.forEach(node => {
+    let link = document.querySelectorAll(node);
+    link.forEach(el => {
+      let newLink = linkifyStr(el.textContent);
+      el.innerHTML = newLink;
+    });
+  });
 }
 
-
 // Start the whole thing.
-objectHandler(res);
+objectHandler(resume);
 
 // Place those nodes.
 nodePlacer(layout);
@@ -127,4 +127,4 @@ nodePlacer(layout);
 linkifier();
 
 // Set the page title
-document.title = res.name + ' - Resume';
+document.title = res.name + " - Resume";
