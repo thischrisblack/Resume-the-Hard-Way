1cd9630fbcfc2a3b591715399d0542b297e2f520
Chris Black <black@chrisblack.net>
2020-08-30 17:33:59 -0400

    Updated resume.

--- src/index.js
+++ src/index.js
@@ -8,15 +8,15 @@ const linkifyStr = require("linkifyjs/string");
  * @param {element} [parent=document.querySelector('body')] - The parent node to be passed into the next function.
  */
 function objectHandler(object, parent = document.querySelector("body")) {
-  let keys = Object.getOwnPropertyNames(object);
+    let keys = Object.getOwnPropertyNames(object);
 
-  keys.forEach(key => {
-    if (Array.isArray(object[key])) {
-      arrayHandler(object[key], key, parent);
-    } else {
-      nodeAppender(object[key], key, parent);
-    }
-  });
+    keys.forEach((key) => {
+        if (Array.isArray(object[key])) {
+            arrayHandler(object[key], key, parent);
+        } else {
+            nodeAppender(object[key], key, parent);
+        }
+    });
 }
 
 /**
@@ -27,21 +27,21 @@ function objectHandler(object, parent = document.querySelector("body")) {
  * @param {element} [type=div] - The type of created DOM node.
  */
 function nodeAppender(element, key, parent, type = "div") {
-  const newNode = document.createElement(type);
-  // Set element's class to the key (e.g. 'name' or 'skill').
-  newNode.setAttribute("class", key);
-  // If it's a list item, add hider class.
-  if (type === "li") {
-    newNode.classList.add("hider");
-  }
-  parent.appendChild(newNode);
-  if (typeof element === "object") {
-    // Objects recurse, with the parent updated.
-    objectHandler(element, newNode);
-  } else {
-    // If it's not an object, it's ready to display.
-    newNode.innerHTML = element;
-  }
+    const newNode = document.createElement(type);
+    // Set element's class to the key (e.g. 'name' or 'skill').
+    newNode.setAttribute("class", key);
+    // If it's a list item, add hider class.
+    if (type === "li") {
+        newNode.classList.add("hider");
+    }
+    parent.appendChild(newNode);
+    if (typeof element === "object") {
+        // Objects recurse, with the parent updated.
+        objectHandler(element, newNode);
+    } else {
+        // If it's not an object, it's ready to display.
+        newNode.innerHTML = element;
+    }
 }
 
 /**
@@ -51,29 +51,29 @@ function nodeAppender(element, key, parent, type = "div") {
  * @param {element} parent - The parent node of the newly crated <ul> element.
  */
 function arrayHandler(array, key, parent) {
-  const newList = document.createElement("ul");
-  newList.setAttribute("class", key);
-  parent.appendChild(newList);
+    const newList = document.createElement("ul");
+    newList.setAttribute("class", key);
+    parent.appendChild(newList);
 
-  array.forEach(el => {
-    // Each element is appended as a new <li> node to the parent <ul>
-    nodeAppender(el, key, newList, "li");
-  });
+    array.forEach((el) => {
+        // Each element is appended as a new <li> node to the parent <ul>
+        nodeAppender(el, key, newList, "li");
+    });
 }
 
 /**
  * Newly-created elements of class [0] are to be appended to element [1].
  */
 const layout = [
-  [".name", "name"],
-  [".address", "address"],
-  [".position", "name"],
-  [".contact", "contact"],
-  [".experience", "main"],
-  [".projects", "main"],
-  [".portfolio", "aside"],
-  [".skills", "aside"],
-  [".footer", "footer"]
+    [".name", "name"],
+    [".address", "address"],
+    [".position", "name"],
+    [".contact", "contact"],
+    [".experience", "main"],
+    [".projects", "main"],
+    [".portfolio", "aside"],
+    [".skills", "aside"],
+    [".footer", "footer"],
 ];
 
 /**
@@ -81,58 +81,58 @@ const layout = [
  * @param {Array} layout - An array of class/element pairs.
  */
 function nodePlacer(layout) {
-  layout.forEach(layoutPair => {
-    let parent = document.querySelector(layoutPair[1]);
-    let child = document.querySelector(layoutPair[0]);
-    // First create section header from each class name.
-    nodeAppender(
-      child.className,
-      "section-header section-header-" + child.className,
-      parent,
-      "h1"
-    );
-    // Then append the rest of the nodes.
-    parent.appendChild(child);
-  });
+    layout.forEach((layoutPair) => {
+        let parent = document.querySelector(layoutPair[1]);
+        let child = document.querySelector(layoutPair[0]);
+        // First create section header from each class name.
+        nodeAppender(
+            child.className,
+            "section-header section-header-" + child.className,
+            parent,
+            "h1"
+        );
+        // Then append the rest of the nodes.
+        parent.appendChild(child);
+    });
 }
 
 /**
  * Linkify link strings.
  */
 function linkifier() {
-  // Linkify text in these classes
-  const linkNodes = [
-    ".email",
-    ".portfolio",
-    ".project__link",
-    ".note",
-    ".footer"
-  ];
+    // Linkify text in these classes
+    const linkNodes = [
+        ".email",
+        ".portfolio",
+        ".project__link",
+        ".note",
+        ".footer",
+    ];
 
-  linkNodes.forEach(node => {
-    let link = document.querySelectorAll(node);
-    link.forEach(el => {
-      let newLink = linkifyStr(el.textContent);
-      el.innerHTML = newLink;
+    linkNodes.forEach((node) => {
+        let link = document.querySelectorAll(node);
+        link.forEach((el) => {
+            let newLink = linkifyStr(el.textContent);
+            el.innerHTML = newLink;
+        });
     });
-  });
 }
 
 /**
  * Make projects or skills hide-able by clicking on them.
  */
 function enableHideableNodes() {
-  let hideableNodes = document.querySelectorAll(".hider");
-  hideableNodes.forEach(node => {
-    node.addEventListener("click", e => {
-      // If it's an <li>, hide it, if not, hide its parent.
-      if (e.target.nodeName === "LI") {
-        e.target.style.display = "none";
-      } else {
-        e.target.parentNode.style.display = "none";
-      }
+    let hideableNodes = document.querySelectorAll(".hider");
+    hideableNodes.forEach((node) => {
+        node.addEventListener("click", (e) => {
+            // If it's an <li>, hide it, if not, hide its parent.
+            if (e.target.nodeName === "LI") {
+                e.target.style.display = "none";
+            } else {
+                e.target.parentNode.style.display = "none";
+            }
+        });
     });
-  });
 }
 
 // Start the whole thing.
